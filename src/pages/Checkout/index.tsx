e702a5259ec470063d5254fcb304c5e9d747d1ab
Bernardo Caetano <bernardocaetano@lancenet.com.br>
2023-03-01 09:14:01 -0300

    doing layout of checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,9 +1,41 @@
-import { CheckoutContainer } from './styles'
+import { CurrencyDollar, MapPinLine } from 'phosphor-react'
+import { AskContainer, CheckoutContainer, CoffeeSelectedContainer, CoffeeSelectedContent, FormContainer, FormTitle, IconContainer, InfoContent, PaymentContainer, Title } from './styles'
 
 export function Checkout() {
   return (
     <CheckoutContainer>
-      <h1>Checkout</h1>
+      <AskContainer>
+        <Title><h2>Complete seu pedido</h2></Title>
+        <FormContainer>
+          <FormTitle>
+            <IconContainer>
+              <MapPinLine color='#C47F17' size={22}/>
+            </IconContainer>
+            <InfoContent>
+              <p>Endereço de Entrega</p>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </InfoContent>
+          </FormTitle>
+        </FormContainer>
+        <PaymentContainer>
+        <FormTitle>
+            <IconContainer>
+              <CurrencyDollar color='#8047F8' size={22}/>
+            </IconContainer>
+            <InfoContent>
+              <p>Pagamento</p>
+              <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+            </InfoContent>
+          </FormTitle>
+        </PaymentContainer>
+      </AskContainer>
+
+      <CoffeeSelectedContainer>
+        <Title><h2>Cafés selecionados</h2></Title>
+        <CoffeeSelectedContent></CoffeeSelectedContent>
+      </CoffeeSelectedContainer>
+      
+      
     </CheckoutContainer>
   )
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,8 +1,112 @@
 import styled from 'styled-components'
 
-export const CheckoutContainer = styled.div`
+const BaseContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+`
+
+export const CheckoutContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
   width: 100%;
+  gap: 2rem;
+  max-width: 70rem;
+
+`
+
+export const AskContainer = styled(BaseContainer)`
+  flex-direction: column;
+  gap: 0.75rem;
+`
+
+export const Title = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: 0.25rem;
+  width: 100%;
+
+  h2 {
+    align-self: flex-start;
+    font-family: 'Baloo 2';
+    font-weight: 700;
+    font-size: 1.125rem;
+    line-height: 130%;
+    color: ${props => props.theme['base-subtitle']};
+  }
+`
+
+export const FormContainer = styled(BaseContainer)`
+  padding: 2.5rem;
+  width: 40rem;
+  height: 23.25rem;
+  background: ${props => props.theme['base-card']};
+  border-radius: 6px;
+  flex: none;
+  order: 0;
+  align-self: stretch;
+  flex-grow: 0;
+`
+
+export const PaymentContainer = styled(BaseContainer)`
+  padding: 2.5rem;
+  width: 40rem;
+  height: 12.9375rem;
+  background: ${props => props.theme['base-card']};
+  border-radius: 6px;
+  flex: none;
+  order: 1;
+  align-self: stretch;
+  flex-grow: 0;
+`
+export const CoffeeSelectedContainer = styled(BaseContainer)`
+  flex-direction: column;
+  width: 100%;
+  gap: 0.75rem;
+`
+
+export const CoffeeSelectedContent = styled(BaseContainer)`
+  padding: 2.5rem;
+  width: 28rem;
+  height: 23.125rem;
+  background: ${props => props.theme['base-card']};
+  border-radius: 6px 44px;
+`
+export const FormTitle = styled(BaseContainer)`
+  height: 2.75rem;
+`
+
+export const IconContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  height: 100%;
+`
+
+export const InfoContent = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  gap: 0.125rem;
+
+  p{
+    line-height: 130%;
+    align-self: stretch;
+    flex-grow: 0;
+  }
+
+  p:first-child {
+    color:  ${props => props.theme['base-subtitle']};
+    order: 0;
+  }
+
+  p:last-child {
+    font-size: 0.875rem;
+    color: ${props => props.theme['base-text']};
+    flex: none;
+    order: 1;
+  }
 `
